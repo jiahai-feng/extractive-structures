ccae6c97f766db6d4656eca6dc349df585b9da5f
Jiahai Feng <fjiahai@berkeley.edu>
2025-04-01 09:53:20 -0700

    reverts unwanted changes

--- slurm/eval_ocr.sh
+++ slurm/eval_ocr.sh
@@ -1,14 +1,9 @@
-# List of config files
-CONFIG_FILES=(
-    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_1e-05_12.yaml"
-    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_1e-05_16.yaml"
-    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_3e-05_12.yaml"
-    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_3e-05_16.yaml"
-)
+#!/bin/bash
+#SBATCH --output=jobs/eval_ocr-%j.out
+#SBATCH -c 16
+#SBATCH -p jsteinhardt
 
-# Loop through each config file and run the command
-for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
-    echo "Processing config file: $CONFIG_FILE"
-    uv run python -m extractive_structures.scripts.eval_ocr \
-        --config "$CONFIG_FILE"
-done
+cd /data/fjiahai/extractive_structures
+
+uv run python -m extractive_structures.scripts.eval_ocr \
+    --config $CONFIG_FILE
