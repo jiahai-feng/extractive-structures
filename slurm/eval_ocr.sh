fe26bcf25553a62d0832ceaf5c113933689c527a
Jiahai Feng <fengjiahai@gmail.com>
2025-04-01 16:48:04 +0000

    changes to viz and readme

--- slurm/eval_ocr.sh
+++ slurm/eval_ocr.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-#SBATCH --output=jobs/eval_ocr-%j.out
-#SBATCH -c 16
-#SBATCH -p jsteinhardt
+# List of config files
+CONFIG_FILES=(
+    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_1e-05_12.yaml"
+    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_1e-05_16.yaml"
+    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_3e-05_12.yaml"
+    "/home/ubuntu/extractive_structures/paper_experiments/sweep_all/full_gemma_27b_3e-05_16.yaml"
+)
 
-cd /data/fjiahai/extractive_structures
-
-uv run python -m extractive_structures.scripts.eval_ocr \
-    --config $CONFIG_FILE
+# Loop through each config file and run the command
+for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
+    echo "Processing config file: $CONFIG_FILE"
+    uv run python -m extractive_structures.scripts.eval_ocr \
+        --config "$CONFIG_FILE"
+done
